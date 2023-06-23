96249e48860f09517fd51094e17a7d60fe177831
SegundoMarotta <segunmarotta@hotmail.com>
2023-06-22 21:28:38 -0300

    Testing

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,11 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   if(x > y){
+      return x;
+   }else if (x < y){
+      return y;
+   }else return x
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +17,9 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if (edad >= 18){
+      return "Allowed";
+   }else {return "Not allowed"};
 }
 
 function conection(status) {
@@ -21,6 +29,13 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   if (status === 1){
+      return "Online"
+   }else if (status === 2){
+      return "Away";
+   }else {
+      return "Offline"
+   }
 }
 
 function saludo(idioma) {
